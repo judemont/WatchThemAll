ed490a30cdd8d399a203d494481b33f3449bdf4f
JdM <julien@rmbi.ch>
2024-04-12 18:05:44 +0200

    search

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, redirect
 import os
 import json
+import difflib
+
 
 app = Flask(__name__)
 app.secret_key = "suuuuuper secret key (;"
@@ -19,22 +21,38 @@ LANGUAGES = [
     ('ru-RU', 'Russian')
 ]
 
-with open("./movies.json", "r") as file:
-    moviesData = json.load(file)
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def home():
+
+    with open("./movies.json", "r") as file:
+        moviesData = json.load(file)
+
+
     if(not "languageCode" in session):
         session["languageCode"] = "en-EN"
-    print(session["languageCode"])
 
-    moviesDataInLanguage = [movieData for movieData in moviesData if movieData["language_code"].startswith(session["languageCode"].split("-")[0])]
+    moviesData = [movieData for movieData in moviesData if movieData["language_code"].startswith(session["languageCode"].split("-")[0])]
 
-    return render_template('home.html', moviesData=moviesDataInLanguage, moviesDataLength=len(moviesDataInLanguage), languages=LANGUAGES, languageCode=session["languageCode"])
+    if("q" in request.args):
+        searchQuery = request.args.get('q')
+
+        for i in range(len(moviesData)):
+            similarity = difflib.SequenceMatcher(None, moviesData[i]["title"], searchQuery).ratio()
+            moviesData[i]["searchSimilarity"] = similarity
+        
+        moviesData = sorted(moviesData, key=lambda x: x["searchSimilarity"], reverse=True)
+        print(moviesData)
+
+
+    return render_template('home.html', moviesData=moviesData, moviesDataLength=len(moviesData), languages=LANGUAGES, languageCode=session["languageCode"])
 
 
 @app.route('/movie/<index>/')
 def movie(index):
+    with open("./movies.json", "r") as file:
+        moviesData = json.load(file)
     return render_template('movie.html', movieData=moviesData[int(index)])
 
 
